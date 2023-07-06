5721864cea823872f439916bbfb199824c8a6d4b
JTawdawd <sac18fxu@uea.ac.uk>
2023-07-06 23:09:26 +0100

    learning how to interact with psql

--- handlers.go
+++ handlers.go
@@ -25,3 +25,47 @@ func HandleLogin(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Login request received"))
 }
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var req LoginRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	log.Printf("Received create request:\nUsername: %s\nPassword: %s\n", req.Username, req.Password)
+
+	stmt, err := DB.Prepare("INSERT INTO user_account(username, password) VALUES($1, $2)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec(req.Username, req.Password)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Login request received"))
+}
+
+func getUser1() {
+	var (
+		id       int
+		username string
+	)
+	rows, err := DB.Query("select id, username from user_account where id = $1", 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&id, &username)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, username)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/handlers"
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "postgres"
+)
+
+var DB *sql.DB
+
 type server struct{}
+
+func connectDB() {
+	pgsqlDetails := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, os.Getenv("DB_PASSWORD"), dbname)
+	db, err := sql.Open("postgres", pgsqlDetails)
+	if err != nil {
+		panic(err)
+	}
+	DB = db
+}
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -16,6 +40,13 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading environment variables file")
+	}
+	connectDB()
+	getUser1()
+
 	corsMiddleware := handlers.CORS(
 		handlers.AllowedOrigins([]string{"http://localhost:3000"}),
 		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
@@ -24,5 +55,6 @@ func main() {
 	s := &server{}
 	http.Handle("/", corsMiddleware(s))
 	http.HandleFunc("/login", HandleLogin)
+	http.HandleFunc("/create", CreateUser)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
